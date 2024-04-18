ac0dd0da81ff57f82eebe525ba0d594dfe4a623a
Shaun Barnes <shaun.w.barnes@gmail.com>
2024-04-18 16:35:17 -0400

    Assignment 6

---
+++ Assignment 6/MainNum.py
@@ -0,0 +1,14 @@
+from lib1 import *
+
+a = int(input("Please enter a number: "))
+b = int(input("Please enter a number: "))
+c = int(input("Please enter a number: "))
+d = int(input("Please enter a number: "))
+
+result = CompareNumbers(a, b, c, d)
+
+print()
+
+print("The largest number is",  result)
+
+print()
---
+++ Assignment 6/lib1.py
@@ -0,0 +1,14 @@
+def CompareNumbers(num1, num2, num3, num4):
+    largest = num1
+
+    if ( num2 > largest):
+        largest = num2
+    
+    if ( num3 > largest):
+        largest = num3
+
+    if ( num4 > largest):
+        largest = num4
+    
+    return largest
+
